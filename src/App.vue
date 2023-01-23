4e4c4b87e1768d73734a894b8816b1b36b502010
lenkaar <lenkaar@gmail.com>
2023-01-23 16:53:54 +0530

    vue-project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="total">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  // components: {},
+};
 </script>
 
 <style>
@@ -20,7 +18,16 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e50; 
+}
+.total{
+  border-style: double;
+  border-radius: 10px;
+  padding: 20px 0;
+  margin: 30px 250px;
+}
+.total img{
+  width: 20%;
+  height: 20%;
 }
 </style>
